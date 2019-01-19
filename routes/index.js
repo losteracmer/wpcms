bbc96e1c30ee5f9f0d24f75494cc4114e2c184f7
sst <445247721@qq.com>
2019-01-19 10:42:09 +0800

    写了一个超长的触发器

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,3 @@ const conn = require('../common/mysql');
 let sql = 'select * from customer';
 
 
-/* GET home page. */
-module.exports = (app)=>{
-  app.get('/',(req, res)=>{
-    res.render('index',{title:'Express'});
-  });
-  app.get('/from',(req, res)=>{
-    conn.query('select * from customer').then(rows=>{
-        // for(let i = 0 ; i < rows.length; ++i){
-        //   console.log(rows[i].real_name);
-        // }
-        res.render('from',{data:rows}); 
-    }).catch(error =>{
-      console.error(error);
-    });
-  });
-};
